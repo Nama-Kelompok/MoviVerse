74636358282e6399930a04a5d27775f95150bcf5
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-11-30 21:30:23 +0700

    Try fetch data from localhost

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from main.views import movie_page, search_movies, get_movie_data
+from . import views
 
 app_name = 'main'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('', search_movies, name='search_movies'),
     path('wiki/<str:id>', movie_page, name='movie_page'),
     path('entity/<str:id>', get_movie_data, name='get_movie_data'),
+    path("movie/<path:uri>/", views.get_movie_details, name="movie_detail"),
 ]
